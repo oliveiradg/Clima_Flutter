e2be70797850b764e65900d7f907f175cb0d4aa3
João Oliveira <j.oliveiradg@gmail.com>
2023-01-29 21:49:43 -0300

    [DIA 21] feat: Criação do Arquivo de configuração de Localização

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,49 +1,25 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
-
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   void initState() {
     super.initState();
     getLocation();
   }
-  
-  
 
   Future getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    print(position);
-
-
+    Location location = Location();
+    await location.getCurrentLocation();
   }
-
-
-
-
-
-
-
-
-
 
   @override
   Widget build(BuildContext context) {
-    
-   
-
-
-    return Scaffold(
-
-
-    );
+    return Scaffold();
   }
 }
-
-
---
+++ lib/services/location.dart
@@ -0,0 +1,30 @@
+
+
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  Location({this.latitude, this.longitude});
+
+  double latitude;
+  double longitude;
+
+  Future<void> getCurrentLocation () async {
+      try{
+      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+
+      latitude = position.latitude;
+      longitude = position.longitude; 
+
+      
+
+
+    }
+    catch(e){
+      print(e);
+    }
+  }
+
+
+
+
+}
